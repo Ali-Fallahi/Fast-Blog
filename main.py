854391f995e6f90dd59fb0cd10a712949f77dc4f
ali fallahi <fallahia80@gmail.com>
2025-08-24 11:42:03 +0330

    blog app and db created

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {"data": f"{limit} published blogs form the db"}
+    else:
+        return {"data": f"{limit} blogs form the db"}
 
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id, limit: int = 10):
+    return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data": f"blog is created with title as {blog.title}"}
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
