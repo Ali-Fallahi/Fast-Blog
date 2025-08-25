4b036f3b26ef8e6e21acaf3a5007d1eeeb260f69
ali fallahi <fallahia80@gmail.com>
2025-08-25 22:24:34 +0330

    user create and get by id

--- blog/schemas.py
+++ blog/schemas.py
@@ -5,8 +5,24 @@ class Blog(BaseModel):
     title: str
     body: str
 
+
 class ShowBlog(BaseModel):
     title: str
     body: str
+
+    class Config:
+        orm_mode = True
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
     class Config:
         orm_mode = True
