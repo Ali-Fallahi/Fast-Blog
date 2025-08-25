6b3aafedda8908adba2f3f21427a5c5a92c8bb17
ali fallahi <fallahia80@gmail.com>
2025-08-25 21:37:38 +0330

    add schema for get requests

--- blog/main.py
+++ blog/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, HTTPException, status, Depends, Response
 from sqlalchemy.orm import Session
 from . import schemas, models
@@ -27,18 +29,31 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if blog.first() is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 
-@app.get("/blog")
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(request.model_dump())
+    db.commit()
+    return 'updated'
+
+
+@app.get("/blog", response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).get(id)
     if not blog:
